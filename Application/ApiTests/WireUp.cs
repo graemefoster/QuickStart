393d294340c03e6378983605f4f51e3c09e45c13
graemefoster <graemefoster@microsoft.com>
2021-10-14 15:20:40 +0800

    Adding tests

---
+++ Application/ApiTests/WireUp.cs
@@ -0,0 +1,15 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace ApiTests
+{
+    public class WireUp
+    {
+        [Fact]
+        public void Test1Is1()
+        {
+            1.ShouldBe(1);
+        }
+    }
+}
