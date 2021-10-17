59a8c77f325264ccb03f357fd9f7eccec7a5183f
graemefoster <graemefoster@microsoft.com>
2021-10-17 20:00:04 +0800

    Checking again

--- Application/DatabaseUpgrades/configure-database.sql
+++ Application/DatabaseUpgrades/configure-database.sql
@@ -1,7 +1,7 @@
 ﻿-- Script to set up initial users for the database.
-IF NOT EXISTS(SELECT * from sys.database_principals WHERE NAME = '${{ env.MANAGED_IDENTITY_NAME }}.microsoft.com')
-    CREATE USER [${{ env.MANAGED_IDENTITY_NAME }}.microsoft.com] FROM EXTERNAL PROVIDER;
+IF NOT EXISTS(SELECT * from sys.database_principals WHERE NAME = '${{ env.MANAGED_IDENTITY_NAME }}@microsoft.com')
+    CREATE USER [${{ env.MANAGED_IDENTITY_NAME }}@microsoft.com] FROM EXTERNAL PROVIDER;
 
-EXEC sp_addrolemember 'db_owner', '${{ env.MANAGED_IDENTITY_NAME }}.microsoft.com'
+EXEC sp_addrolemember 'db_owner', '${{ env.MANAGED_IDENTITY_NAME }}@microsoft.com'
 
-print 'Added user ${{ env.MANAGED_IDENTITY_NAME }}.microsoft.com as db_owner to database'
+print 'Added user ${{ env.MANAGED_IDENTITY_NAME }}@microsoft.com as db_owner to database'
