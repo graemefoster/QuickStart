a959a20b3f020eb61eb953b4a5211670bece0b65
graemefoster <graemefoster@microsoft.com>
2021-11-03 21:18:32 +0800

    This might just work...

--- Application/StaticApp/sample-react-app/src/components/SignInButton.tsx
+++ Application/StaticApp/sample-react-app/src/components/SignInButton.tsx
@@ -1,19 +1,25 @@
 import { useMsal } from "@azure/msal-react";
-import { loginRequest } from "../authConfig";
-import Button from 'react-bootstrap/Button';
+import { spaConfig } from "../authConfig";
+import Button from "react-bootstrap/Button";
 
 /**
  * Renders a drop down button with child buttons for logging in with a popup or redirect
  */
 export const SignInButton = () => {
-    const { instance } = useMsal();
+  const { instance } = useMsal();
 
-    const handleLogin = () => {
-        instance.loginRedirect(loginRequest).catch(e => {
-            console.log(e);
+  const handleLogin = () => {
+    spaConfig.then((config) => {
+      instance
+        .loginRedirect({ scopes: config.apiConfig.Scopes })
+        .catch((e) => {
+          console.log(e);
         });
-    }
-    return (
-        <Button variant="primary" onClick={() => handleLogin()}>Sign In</Button>
-    )
-}
+    });
+  };
+  return (
+    <Button variant="primary" onClick={() => handleLogin()}>
+      Sign In
+    </Button>
+  );
+};
--- Application/StaticApp/sample-react-app/src/index.tsx
+++ Application/StaticApp/sample-react-app/src/index.tsx
@@ -5,25 +5,26 @@ import App from "./App";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import reportWebVitals from "./reportWebVitals";
-import { msalConfig } from "./authConfig";
+import { spaConfig } from "./authConfig";
 import "bootstrap/dist/css/bootstrap.css";
 
 /**
  * Initialize a PublicClientApplication instance which is provided to the MsalProvider component
  * We recommend initializing this outside of your root component to ensure it is not re-initialized on re-renders
  */
-const msalInstance = new PublicClientApplication(msalConfig);
+spaConfig.then((config) => {
+  const msalInstance = new PublicClientApplication(config.msalConfig);
+  ReactDOM.render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+});
