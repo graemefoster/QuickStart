f4367583de0d806e7dc217ec2212b3fc97275021
graemefoster <graemefoster@microsoft.com>
2021-11-05 21:25:32 +0800

    Enable CORS for all routes... will lock this behind apim in time

--- Application/MicroService/index.js
+++ Application/MicroService/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/resource", (req, res) => {
   res.send({ name: "Graeme", pets: ["dog"] });
